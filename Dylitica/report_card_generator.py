e6044448aedbfe3506f35864e6da2fef5a6fa1e4
purnimapant77 <purnimaaaa7@gmail.com>
2026-06-02 13:00:12 +0545

    Student report card generator

---
+++ Dylitica/report_card_generator.py
@@ -0,0 +1,14 @@
+#Student report card generator
+print("Student report card generator:")
+name=input("Enter the name of student:")
+roll_no=int(input("Enter roll number of student:"))
+print(f"Enter marks of {name} out of 100 in following subject:")
+science=int(input("Enter obtained marks in Science:"))
+maths=int(input("Enter obtained marks in maths:"))
+computer=int(input("Enter obtained marks in Computer:"))
+
+student={
+    "student_name":name,
+    "rollno":roll_no
+    marks
+}
